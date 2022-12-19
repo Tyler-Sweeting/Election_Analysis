ad35f0bec31e21d2138b1d600cbdd892c0a4f2ce
Tyler Sweeting <sweetingwt@gmail.com>
2022-12-19 17:09:34 -0500

    Adding a text file to PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -41,26 +41,44 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
-# Determine the percentage of votes for each candidate by looping through the counts
-# Iterate through the candidate list.
-for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # Print the candidate name and percentage of votes.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
-Winning_candidate_summary = (
-    f"-----------------\n"
-    f"Winner: {winning_candidate} \n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-----------------")
-print(Winning_candidate_summary)
-
+    
+    #Save the results to our text file.
+    with open(file_to_save, "w") as txt_file:
+        # Print the final vote count to the terminal.
+        election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+        print(election_results, end="")
+        # Save the final vote count to the text file.
+        txt_file.write(election_results)
+        # Determine the percentage of votes for each candidate by looping through the counts
+        # Iterate through the candidate list.
+        for candidate_name in candidate_votes:
+            # Retrieve vote count of a candidate.
+            votes = candidate_votes[candidate_name]
+            # Calculate the percentage of votes.
+            vote_percentage = float(votes) / float(total_votes) * 100
+            # Print the candidate name and percentage of votes.
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                winning_count = votes
+                winning_percentage = vote_percentage
+                winning_candidate = candidate_name
+            #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            #Set a variable for candidate results
+            candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            # Print each candidate, their voter count, and percentage to the terminal.
+            print(candidate_results)
+            #  Save the candidate results to our text file.
+            txt_file.write(candidate_results)
+        winning_candidate_summary = (
+            f"-----------------\n"
+            f"Winner: {winning_candidate} \n"
+            f"Winning Vote Count: {winning_count:,}\n"
+            f"Winning Percentage: {winning_percentage:.1f}%\n"
+            f"-----------------")
+        print(winning_candidate_summary)
+        # Save the winning candidate's results to the text file.
+        txt_file.write(winning_candidate_summary)
 
--- temp_ifelse.py
+++
@@ -1,7 +0,0 @@
-temperature = int(input("What is the temperatire outside? "))
-
-if temperature > 80:
-    print("Turn on the AC. ")
-
-else:
-    print("Open the windows. ")
